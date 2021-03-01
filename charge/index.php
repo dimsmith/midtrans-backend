6433431dc8e7a937d846c6e66edc558037729ca7
Dimas <73816077+dimsmith@users.noreply.github.com>
2021-03-01 07:00:15 +0700

    Update index.php

--- charge/index.php
+++ charge/index.php
@@ -10,6 +10,7 @@ $api_url = $is_production ?
 
 
 // Check if request doesn't contains `/charge` in the url/path, display 404
+print_r($_SERVER["REQUEST_URI"]);
 if( !strpos($_SERVER['REQUEST_URI'], '/charge') ) {
     http_response_code(404);
     echo "wrong path, make sure it's `/charge`"; exit();
